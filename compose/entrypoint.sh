6e1caea27fb8e7c8a7cc1fbc3cc832db95930e83
Chris Tippett <christippett@Servian-Chris.local>
2016-11-03 10:04:16 +1100

    Go back to using unencrypted http

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -14,8 +14,7 @@ if [ "$1" == "test" ]; then
     runTests
     exit 0
 else
-    openssl req -x509 -newkey rsa:2048 -sha256 -keyout /cert/key.pem -out /cert/cert.crt -days 365 -nodes -subj "/C=AU/ST=Victoria/L=Melbourne/O=Ticket Bounty/OU=Venue API/CN=$1"
-    /usr/local/bin/gunicorn wsgi:app -w 4 -b 0.0.0.0:5000 --certfile=/cert/cert.crt --keyfile=/cert/key.pem --ssl-version 2 --ciphers TLSv1.2 --chdir=/app
+    /usr/local/bin/gunicorn wsgi:app -w 4 -b 0.0.0.0:5000 --chdir=/app
 fi
 
 
