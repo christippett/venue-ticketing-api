caf399685cbe5e4e2ef0b67bf18cdb8c34b04eac
Chris Tippett <christippett@Servian-Chris.local>
2016-11-06 16:11:41 +1100

    Re-arranged project structure and added logging

--- venue/__init__.py
+++ venue/__init__.py
@@ -1,6 +1,68 @@
-from .vif_ticket_array import VIFTicketArray
-from .vif_message import VIFMessage
-from .vif_gateway import VIFGateway, VIFGatewayError
+from functools import wraps
+import logging
+
+from flask import Flask, abort, jsonify, make_response, request  # type: ignore
+from werkzeug.exceptions import HTTPException  # type: ignore
+
+from .vif_gateway import VIFGateway
+
+app = Flask(__name__)
+
+# Configure logging
+handler = logging.StreamHandler()
+handler.setLevel(logging.NOTSET)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
 
 
-__all__ = ['VIFTicketArray', 'VIFMessage', 'VIFGateway', 'VIFGatewayError']
+def validate_gateway_parameters(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        expected_params = ['site_name', 'auth_key', 'agent_no', 'host']
+
+        venue_parameters = {
+            'site_name': request.args.get('site_name'),
+            'auth_key': request.args.get('auth_key'),
+            'agent_no': request.args.get('agent_no'),
+            'host': request.args.get('host')
+        }
+
+        return f(venue_parameters=venue_parameters, *args, **kwargs)
+    return decorator
+
+
+@app.errorhandler(Exception)
+@app.errorhandler(400)
+@app.errorhandler(404)
+@app.errorhandler(500)
+def error_view(error):
+    code = 500 if not isinstance(error, HTTPException) else error.code
+    return jsonify({
+        'status_code': error.code,
+        'response': str(error),
+        'error': error.description
+    }), code
+
+
+@app.route('/api/get_data', methods=['GET'])
+@validate_gateway_parameters
+def get_tasks(venue_parameters):
+    gateway = VIFGateway(**venue_parameters)
+    data = gateway.get_data()
+    return jsonify({
+        'venue': venue_parameters,
+        'data': data
+    })
+
+
+@app.route('/api/handshake', methods=['GET'])
+@validate_gateway_parameters
+def handshake(venue_parameters):
+    gateway = VIFGateway(**venue_parameters)
+    data = gateway.handshake()
+    return jsonify({
+        'venue': venue_parameters,
+        'data': data
+    })
