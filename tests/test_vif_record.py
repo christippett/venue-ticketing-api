1955a123b6fbd13d085b62c09cdeff0e029a3e1a
Chris Tippett <c.tippett@gmail.com>
2017-03-09 22:02:41 +1100

    Add tests for formatting key-value data with english field names

--- tests/test_vif_record.py
+++ tests/test_vif_record.py
@@ -24,14 +24,14 @@ def test_extract_record_code_from_raw_content():
 
 
 def test_named_data_fields_convert_to_integer_keys():
-    data_content = {
+    data = {
         'site_name': 'BARKER',
         'packet_id': 'ABCD',
         'request_code': '1',
         'comment': 'Test',
         'auth_info': '108193016648'
     }
-    message = VIFRecord(record_code='vrq', data=data_content)
+    message = VIFRecord(record_code='vrq', data=data)
     assert message.data == {
         1: 'BARKER',
         2: 'ABCD',
@@ -60,14 +60,14 @@ def test_data_fields_not_parsed_if_keys_already_integers():
 
 
 def test_formatted_content_1():
-    data_content = {
+    data = {
         'site_name': 'BARKER',
         'packet_id': 'ABCD',
         'request_code': '1',
         'comment': 'Test',
         'auth_info': '108193016648'
     }
-    message = VIFRecord(record_code='vrq', data=data_content)
+    message = VIFRecord(record_code='vrq', data=data)
     assert message.content() == '{vrq}{1}BARKER{2}ABCD{3}1{4}Test{8}108193016648'
 
 
@@ -108,7 +108,7 @@ def test_formatted_content_excludes_record_code_when_not_available_2():
 
 
 def test_unable_to_format_content_if_record_code_not_provided():
-    data_content = {
+    data = {
         'site_name': 'BARKER',
         'packet_id': 'ABCD',
         'request_code': '1',
@@ -116,4 +116,46 @@ def test_unable_to_format_content_if_record_code_not_provided():
         'auth_info': '108193016648'
     }
     with pytest.raises(Exception):
-        VIFRecord(data=data_content)
+        VIFRecord(data=data)
+
+
+def test_mapping_of_data_to_field_names_1():
+    data = {
+        'site_name': 'BARKER',
+        'packet_id': 'ABCD',
+        'request_code': '1',
+        'comment': 'Test',
+        'auth_info': '108193016648'
+    }
+    message = VIFRecord(record_code='vrq', data=data)
+    assert message.format_data() == data
+
+
+def test_mapping_of_data_to_field_names_2():
+    data = {
+        1: 'BARKER',
+        2: 'ABCD',
+        3: '1',
+        4: 'Test',
+        8: '108193016648'
+    }
+    message = VIFRecord(record_code='vrq', data=data)
+    assert message.format_data() == {
+        'site_name': 'BARKER',
+        'packet_id': 'ABCD',
+        'request_code': '1',
+        'comment': 'Test',
+        'auth_info': '108193016648'
+    }
+
+
+def test_mapping_of_data_to_field_names_3():
+    raw_content = '{vrq}{1}BARKER{2}ABCD{3}1{4}Test{8}108193016648'
+    message = VIFRecord(raw_content=raw_content)
+    assert message.format_data() == {
+        'site_name': 'BARKER',
+        'packet_id': 'ABCD',
+        'request_code': '1',
+        'comment': 'Test',
+        'auth_info': '108193016648'
+    }
