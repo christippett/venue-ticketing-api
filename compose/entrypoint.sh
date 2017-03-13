e001a866f16dbe90e2bf90a9fd2691b059698fca
Chris Tippett <c.tippett@gmail.com>
2017-03-13 21:52:43 +1100

    Update test function to return code 0 on any test error (the build should fail as a result of this commit)

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 function runTests() {
+    FAILED_TEST=0
     cd /app
     echo "CHECKING TYPINGS..."
     mypy run.py
+    if (( $? != 0 )); then (( FAILED_TEST+=1 )); fi
+
     echo "CHECKING PEP8..."
     pep8 **/*.py
+    if (( $? != 0 )); then (( FAILED_TEST+=1 )); fi
+
     echo "RUNNING TEST SUITE..."
     pytest
+    if (( $? != 0 )); then (( FAILED_TEST+=1 )); fi
+
+    if (( $FAILED_TEST > 0 )); then return 1; else return 0; fi
 }
 
 if [ "$1" == "test" ]; then
